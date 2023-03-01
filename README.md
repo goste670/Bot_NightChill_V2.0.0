3e5dd872d43a9f4a69c45657ce85b286fff490b1
Ada2point0 <adam.naceur@gmail.com>
2023-03-01 01:02:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bot_NightChill_V2.0
+ v2.0
---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: 'OTU5MDkwOTA1NDYwMTIxNjIx.GLpp4U.0hAWb9OGlp8wg3NsF4aKo44YuelVSvC68AcbHE',
+        owners: '262294964438827009',
+        funny: '262294964438827009',
+        color: '#FF0000',
+        footer: 'sltcv ?',
+        maxserver: '4',
+        maxVol: '150',
+        everyoneMention: false,
+        hostedBy: true,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+}
